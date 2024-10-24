1679a673111b9abb3d5d0fc09ef046c09b54166e
saikumargothrala <113005466+saikumargothrala@users.noreply.github.com>
2024-10-24 12:05:44 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try (Connection conn = Database.getConnection()) {
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String role = rs.getString("role");
+                HttpSession session = request.getSession();
+                session.setAttribute("username", username);
+                session.setAttribute("role", role);
+
+                if (role.equals("Employee")) {
+                    response.sendRedirect("requestAccess.jsp");
+                } else if (role.equals("Manager")) {
+                    response.sendRedirect("pendingRequests.jsp");
+                } else {
+                    response.sendRedirect("createSoftware.jsp");
+                }
+            } else {
+                response.sendRedirect("login.jsp");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
